c61e53e320051168703dad5c379896406c3b331a
施宇 <shiyu2450@gmail.com>
2019-03-23 20:44:39 +0800

    增加php的服务

---
+++ c.php
@@ -0,0 +1,57 @@
+<?php
+
+//终端shell通信
+$STDIN = fopen('php://stdin');
+$STDOUT = fopen('php://stdout');
+
+function main (){
+    //创建一个socket套接流
+    $socket = socket_create(AF_INET6,SOCK_STREAM,SOL_TCP);
+    /****************设置socket连接选项，这两个步骤你可以省略*************/
+    //接收套接流的最大超时时间1秒，后面是微秒单位超时时间，设置为零，表示不管它
+    //socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array("sec" => 1, "usec" => 0));
+    //发送套接流的最大超时时间为6秒
+    //socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array("sec" => 6, "usec" => 0));
+    /****************设置socket连接选项，这两个步骤你可以省略*************/
+
+    //连接服务端的套接流，这一步就是使客户端与服务器端的套接流建立联系
+    if(socket_connect($socket,'[::1]', 404) == false){
+        fwrite($STDOUT, '链接失败:'.socket_strerror(socket_last_error())."\n");
+        return 1;
+    }else{
+        fwrite($STDOUT, '请输入需要发送的数据'."\n");
+        for(;;){
+            //获取终端用户输入
+            $message = fgets($STDIN);
+            if(stripos($message, '关闭') !== false) {
+                fwrite($STDOUT, '用户关闭通讯'."\n");
+                break;
+            }
+
+            //向服务端写入字符串信息
+            if(socket_write($socket,$message,strlen($message)) == false){
+                fwrite($STDOUT, '发送数据失败:'.socket_strerror(socket_last_error())."\n");
+                return 2;
+            }else{
+                fwrite($STDOUT, '发送数据成功:'."\n");
+                //读取服务端返回来的套接流信息--每次读取1024字节数据--直到换行回车字符串结束读完
+                $callback = '';
+                while($callback = socket_read($socket,1024)){
+                    $callback .= $callback;
+                }
+                if($callback === false) {
+                    fwrite($STDOUT, '链接中断:'.socket_strerror(socket_last_error())."\n");
+                }else{
+                    fwrite($STDOUT, '服务端返回数据:'."\n".$callback."\n");
+                }
+            }
+        }
+    }
+    socket_close($socket);//工作完毕，关闭套接流
+    fclose($STDIN);
+    fclose($STDOUT);
+    return 0;
+}
+
+
+return main();
---
+++ s.php
@@ -0,0 +1,75 @@
+<?php
+//终端shell通信
+$STDIN = fopen('php://stdin');
+$STDOUT = fopen('php://stdout');
+
+function child_return(){
+    $return_num = 0;
+    $pid = pcntl_wait($return_num);
+
+    fwrite($STDOUT, '子进程:'.(string)$pid.'退出;返回值:'.(string)$return_num."\n");
+}
+
+function main (){
+    //创建服务端的socket套接流,net协议为IPv4，protocol协议为TCP
+    $socket = socket_create(AF_INET6,SOCK_STREAM,SOL_TCP);
+
+    /*绑定接收的套接流主机和端口,与客户端相对应*/
+    if(socket_bind($socket,'[::]', 404) == false){
+        fwrite($STDOUT, '绑定地址端口失败:'.socket_strerror(socket_last_error())."\n");
+        return 1;
+    }
+    //监听套接流--等待队列8
+    if(socket_listen($socket, 8)==false){
+        fwrite($STDOUT, '监听管道失败:'.socket_strerror(socket_last_error())."\n");
+        return 2;
+    }
+    //注册子进程退出信号
+    pcntl_signal(SIGCHLD, 'child_return');
+
+    //让服务器无限获取客户端传过来的信息
+    do{
+        /*接收客户端传过来的信息*/
+        $accept_resource = socket_accept($socket);
+        /*socket_accept的作用就是接受socket_bind()所绑定的主机发过来的套接流*/
+
+        if($accept_resource !== false){
+            $pid = pcntl_fork();
+            if($pid == 0) {
+
+                //读取客户端传过来的套接流信息--每次读取1024字节数据--直到换行回车字符串结束读完
+                $message = '';
+                while($message = socket_read($accept_resource,1024)){
+                    $message .= $message;
+                }
+                if($message === false) {
+                    fwrite($STDOUT, '链接中断:'.socket_strerror(socket_last_error())."\n");
+                }else{
+                    fwrite($STDOUT, '客户端发送数据:'."\n".$message."\n");
+                    /*socket_write的作用是向socket_create的套接流写入信息，或者向socket_accept的套接流写入信息*/
+                    $return_client = '服务器收到消息为: '.$message."\n";
+                    socket_write($accept_resource,$return_client,strlen($return_client));
+
+                    if(stripos($message, '关闭') !== false) {
+                        $close_connect = '关闭通信';
+                        socket_write($accept_resource,$close_connect,strlen($close_connect));
+                        //socket_close的作用是关闭socket_create()或者socket_accept()所建立的套接流
+                        socket_close($accept_resource);
+                        break;
+                    }
+                }
+            } else {
+                //主进程继续监听端口
+                fwrite($STDOUT, '生成子进程:'.(string)$pid."\n");
+            }
+        }
+    }while(true);
+
+    socket_close($socket);
+
+    return 0;
+}
+
+
+
+return main();
